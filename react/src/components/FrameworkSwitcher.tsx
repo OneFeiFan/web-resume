1b46c4c76ac7eda289b6d9d73c19b2c0cd93dfe6
OneFeiFan <3022504358@qq.com>
2026-08-04 11:20:29 +0800

    🔥 移除未使用的 FrameworkSwitcher 组件

--- react/src/components/FrameworkSwitcher.tsx
+++
@@ -1,8 +0,0 @@
-export default function FrameworkSwitcher() {
-  return (
-    <div className="switcher no-print">
-      <span>⚛ React 版</span>
-      <a href="/vue/" style={{ fontSize: 'var(--text-sm)' }}>Vue 3 版 →</a>
-    </div>
-  );
-}
